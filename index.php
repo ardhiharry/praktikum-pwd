8a09f489e39f0b80a28d3fc5d203aaa12ec9f227
ardhiharry <ardhiharry@gmail.com>
2022-05-31 16:27:54 +0700

    mengubah link Tambah Mahasiswa menjadi button

--- index.php
+++ index.php
@@ -26,10 +26,19 @@
     <div class="container">
       <h1 class="mt-3"">Daftar Mahasiswa</h1>
 
-      <h3><a href="tambah.php">Tambah Mahasiswa</a></h3>
-
+      
       <div class="row">
         <div class="col-md-8">
+          <h3 class="d-flex justify-content-end">
+            <a href="tambah.php" class="btn btn-success">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+              </svg>
+              Tambah Mahasiswa
+            </a>
+          </h3>
+
           <table class="table table-hover">
             <thead class="table-primary">
               <tr>
